f65981171bcd0b19e9509f0bf32b2f9d905ae639
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-06-29 16:36:23 -0400

    removed routes/api and filled seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,37 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
 DROP TABLE IF EXISTS department;
 DROP TABLE IF EXISTS departmentRole;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
-
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id) 
 );
 
 CREATE TABLE departmentRole(
-    id INTEGER PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
+    salary DECIMAL NOT NULL,
     department_id INTEGER
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id)
 )
 
 CREATE TABLE employee(
-    id INTEGER PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
 )
+
+
+SELECT * FROM department;
+SELECT * FROM departmentRole;
+SELECT * FROM employee;
---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+
+USE employeeTracker_DB;
+
+
+
+INSERT INTO department
+    (name)
+VALUES
+
+    ("Human Resources"),
+    ("Payroll"),
+    ("Information Technology"),
+    ("Sales")
+    ("Logistics");
+
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+("manager", 81000.00, 5),
+("i.t technician", 67250.00, 3),
+("payroll analyst", 59000.00, 2),
+("hr assistant", 45500.00, 1),
+("sales person", 75000.00, 4);
+
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+("Tyler", "Keith", 1, NULL),
+("Phil", "Finta", 2, 1),
+("Clay", "Aching", 3, 2),
+("Rob", "Cafo", 5, 2),
+("Tom", "Riddle", 4, 1),
